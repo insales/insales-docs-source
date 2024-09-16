da8a78a5eb175036bb5aed379af233de27ce9a9f
vladimir.ivanin <vladimir.ivanin@insales.ru>
2024-09-16 16:50:05 +0300

    feat(docs): обновление справки шablонов и стилей для виджетов

--- docs/js/extra.js
+++ docs/js/extra.js
@@ -1,5 +1,5 @@
-$(function() {
-  $('.icons-wrapper li').click(function() {
+$(function () {
+  $(".icons-wrapper li").click(function () {
     var $textIcon = $(this).find("p");
     var $temp = $("<input>");
     $("body").append($temp);
@@ -7,11 +7,54 @@ $(function() {
     document.execCommand("copy");
     $temp.remove();
     event.preventDefault();
-    
-    $textIcon.html();    
-    $(this).append('<span class="hint">Тест скопирован!<span>');  
-    if (document.execCommand('copy')) {
-      $(this).find('.hint').fadeOut(600);
-    }  
+
+    $textIcon.html();
+    $(this).append('<span class="hint">Тест скопирован!<span>');
+    if (document.execCommand("copy")) {
+      $(this).find(".hint").fadeOut(600);
+    }
   });
+
+  if ($(".block-templates-table").length > 0) {
+    function formatFields(fields) {
+      return fields
+        .map(
+          (field) =>
+            `<div>
+              <strong>${field.name}</strong> (${field.kind}) <code>${field.handle}</code>
+            </div>`
+        )
+        .join("");
+    }
+    const tableData = blockTemplates.map((template) => ({
+      handle: template.handle,
+      name: template.name,
+      fields: formatFields(template.block_fields),
+    }));
+
+    tableData.forEach((row) => {
+      $(".block-templates-table tbody").append(`
+            <tr>
+                <td>${row.name}</td>
+                <td>${row.handle}</td>
+                <td>${row.fields}</td>
+            </tr>
+          `);
+    });
+
+    $("#block-templates-search-input").on("keyup", function () {
+      var value = $(this).val().toLowerCase();
+      $(".block-templates-table tbody tr").filter(function () {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+      });
+
+      if ($(".block-templates-table tbody tr:visible").length === 0) {
+        $(".block-templates-table tbody").append(`
+          <tr>
+            <td colspan="3" style="text-align: center;">Ничего не найдено</td>
+          </tr>
+        `);
+      }
+    });
+  }
 });
